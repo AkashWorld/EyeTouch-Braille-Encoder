690a85b96f68068aeab6de02bd0d8d6187714552
Khalid Akash <khalkash@gmail.com>
2018-03-22 00:10:08 -0400

    Listing unpaired device needs to be worked on. Scope issues.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 import * as Braille from '../../assets/js/braille.js'
@@ -9,60 +9,93 @@ declare let BrailleKeys:any;
   templateUrl: 'home.html'
 })
   export class HomePage {
+    btStatus = "Not connected."
     BrailleKeys = Braille.GetKeysFromMap(Braille.BrailleMap);
-    constructor(private toastCtrl: ToastController, private bluetoothSerial: BluetoothSerial){
+    DEVICE_NAME = "BRAILLE";
+    brailleDevice = null;
+    isBTEnabled = false;
+    isConnectedToDevice = false;
+    constructor(public toastCtrl: ToastController, public bluetoothSerial: BluetoothSerial){
+      this.DEVICE_NAME = "BRAILLE";
       console.log("HomePage controller")
       console.log(Braille.BrailleMap)
       console.log(this.BrailleKeys)
-      this.bluetoothSerial.isEnabled().then(this.bluetoothSuccess, this.bluetoothFailed)
     }
 
-    bluetoothSuccess(success){
-      console.log("Bluetooth is enabled..." + success)
-      this.presentToast("Bluetooth is enabled..." + success)
-      this.bluetoothSerial.discoverUnpaired().then(this.listUnpairedDevices, this.failedListing)
+    //OnClick()=> Method that turns on bluetooth
+    TurnOnBluetoothOnClick(){
+      this.bluetoothSerial.isEnabled().then(()=>this.DeviceIsEnabled(),()=>this.EnableDevice());
     }
 
 
-    bluetoothFailed(error){
-      console.log("Bluetooth is not enabled with error: " + error + " ...attempting to enabled...")
-      this.presentToast("Bluetooth is not enabled with error: " + error + " ...attempting to enabled...")      
-      this.bluetoothSerial.enable().then(this.bluetoothSuccess, this.bluetoothEnableFailure)
+    EnableDevice(){
+      console.log("Attempting to enable bluetooth...")
+      this.bluetoothSerial.enable().then(()=>this.DeviceIsEnabled(),()=>this.DeviceIsDisabled())
     }
 
-    bluetoothEnableFailure(error){
-      console.log("Bluetooth enable failed due to error: " + error);
-      this.presentToast("Bluetooth enable failed due to error: " + error)
+
+    DeviceIsEnabled(){
+      console.log("Bluetooth is enabled...")
+      this.isBTEnabled = true;
     }
 
-    failedListing(error){
-      console.log("Failed listing unpaired devices..." +error)
-      this.presentToast("Failed listing unpaired devices..." +error)
+    DeviceIsDisabled(){
+      console.log("Bluetooth is disabled...")
+      this.isBTEnabled = false;
     }
 
-    listUnpairedDevices(list){
-      console.log("Found devices...");
-      console.log(list);
-      this.presentToast("Found " + list.size + " devices.");
+    //OnClick() => Find nearby braille devices
+    FindBrailleDeviceOnClick(){
+      if(this.isBTEnabled){
+        this.bluetoothSerial.discoverUnpaired().then((Devices)=>this.FindBrailleDevice(Devices),function(){console.log("Unable to list unpaired devices.")})
+      }
+      else{
+        console.log("Cannot list bluetooth devices, must enable bluetooth first!")
+      }
+    }
+
+    FindBrailleDevice(Devices){
+      console.log("Listing unpaired Bluetooth devices...")
+      for(let i = 0; i < Devices.size; i++){
+        let device = Devices[i]
+        console.log("Name: " + device.name)
+        if(device.name == this.DEVICE_NAME){
+          this.brailleDevice = device;
+          console.log("Braille BT module found!")
+          let retval = this.bluetoothSerial.connect(this.brailleDevice.address).subscribe(()=>this.ConnectedToDevice(), ()=>this.DisonnectedFromDevice());
+        }
+      }
+    }
+
+    ConnectedToDevice(){
+      console.log("Connected to BRAILLE device...")
+      this.isConnectedToDevice = true;
+    }
+
+    DisonnectedFromDevice(){
+      console.log("DIsconnected from BRAILLE device...")
+      this.isConnectedToDevice = false;
     }
 
     OnButtonClick(key){
       console.log('Button pressed: ' + key);
       console.log('Button value: ' + Braille.BrailleMap.get(key));
       this.presentToast('Button value: ' + Braille.BrailleMap.get(key));
+      if(!this.isConnectedToDevice || this.brailleDevice == null){
+        console.log("Not connected to device...")
+        return;
+      }
+      this.bluetoothSerial.write(Braille.BrailleMap.get(key))
     }
+
 
     presentToast(msg){
       let toast = this.toastCtrl.create({
       message: msg,
       duration: 3000,
-      position: 'top'
+      position: 'bottom'
       });
-
-    toast.onDidDismiss(() => {
-      console.log('Dismissed toast');
-    });
-
     toast.present();
     }
   }
+
