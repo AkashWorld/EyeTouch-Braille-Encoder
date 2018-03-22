1568a5dbc7e580eb496bb650b5e6f0de8788fc48
Khalid Akash <khalkash@gmail.com>
2018-03-22 03:40:27 -0400

    Bluetooth comms working. Must fix communication format.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ActionSheetController } from 'ionic-angular';
-import { ToastController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 import * as Braille from '../../assets/js/braille.js'
 declare let BrailleKeys:any;
@@ -9,21 +8,24 @@ declare let BrailleKeys:any;
   templateUrl: 'home.html'
 })
   export class HomePage {
-    btStatus = "Not connected."
     BrailleKeys = Braille.GetKeysFromMap(Braille.BrailleMap);
     DEVICE_NAME = "BRAILLE";
     brailleDevice = null;
     isBTEnabled = false;
     isConnectedToDevice = false;
+    btStatus = "Not connected."
+
     constructor(public toastCtrl: ToastController, public bluetoothSerial: BluetoothSerial){
       this.DEVICE_NAME = "BRAILLE";
       console.log("HomePage controller")
       console.log(Braille.BrailleMap)
       console.log(this.BrailleKeys)
+      this.TurnOnBluetooth();
     }
 
     //OnClick()=> Method that turns on bluetooth
-    TurnOnBluetoothOnClick(){
+    TurnOnBluetooth(){
+      console.log("Initializing bluetooth connection sequence...")
       this.bluetoothSerial.isEnabled().then(()=>this.DeviceIsEnabled(),()=>this.EnableDevice());
     }
 
@@ -46,35 +48,42 @@ declare let BrailleKeys:any;
 
     //OnClick() => Find nearby braille devices
     FindBrailleDeviceOnClick(){
+      this.btStatus = "Attempting to pair with Braille device..."
       if(this.isBTEnabled){
         this.bluetoothSerial.discoverUnpaired().then((Devices)=>this.FindBrailleDevice(Devices),function(){console.log("Unable to list unpaired devices.")})
       }
       else{
         console.log("Cannot list bluetooth devices, must enable bluetooth first!")
+        this.TurnOnBluetooth();
       }
     }
 
     FindBrailleDevice(Devices){
       console.log("Listing unpaired Bluetooth devices...")
-      for(let i = 0; i < Devices.size; i++){
-        let device = Devices[i]
-        console.log("Name: " + device.name)
-        if(device.name == this.DEVICE_NAME){
-          this.brailleDevice = device;
+      console.log(Devices)
+      for(let item in Devices){
+        if(Devices[item].name == this.DEVICE_NAME){
+          this.brailleDevice = Devices[item];
           console.log("Braille BT module found!")
-          let retval = this.bluetoothSerial.connect(this.brailleDevice.address).subscribe(()=>this.ConnectedToDevice(), ()=>this.DisonnectedFromDevice());
+          this.bluetoothSerial.connect(this.brailleDevice.address).subscribe(()=>this.ConnectedToDevice(), ()=>this.DisonnectedFromDevice());
         }
+      }
+      if(this.brailleDevice == null){
+        console.log("Error finding Braille Bluetooth module!")
       }
     }
 
     ConnectedToDevice(){
-      console.log("Connected to BRAILLE device...")
       this.isConnectedToDevice = true;
+      console.log("Connected to BRAILLE device...")
+      this.btStatus = "Braille device connected!"
     }
 
     DisonnectedFromDevice(){
-      console.log("DIsconnected from BRAILLE device...")
       this.isConnectedToDevice = false;
+      this.brailleDevice = null;
+      console.log("Disconnected from BRAILLE device...")
+      this.btStatus = "Not connected."
     }
 
     OnButtonClick(key){
@@ -85,10 +94,14 @@ declare let BrailleKeys:any;
         console.log("Not connected to device...")
         return;
       }
-      this.bluetoothSerial.write(Braille.BrailleMap.get(key))
+      this.btStatus = "Attempting to write: " + key
+      this.bluetoothSerial.write(Braille.BrailleMap.get(key)).then(function(){console.log("Write successful.")},function(){console.log("Write failed.")})
     }
 
-
+/**
+ * Toast UI function method
+ * @param msg Message to put into toast
+ */
     presentToast(msg){
       let toast = this.toastCtrl.create({
       message: msg,
