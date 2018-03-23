de135489f838ae332a94702c7f4592eeefdda837
Khalid Akash <khalkash@gmail.com>
2018-03-22 20:39:01 -0400

    Works with one register.

--- bluetooth/bluetooth_active/bluetooth_active.ino
+++ bluetooth/bluetooth_active/bluetooth_active.ino
@@ -10,27 +10,47 @@ SoftwareSerial BluetoothSerial(BT_SERIAL_TX, BT_SERIAL_RX);
 char buffer[] = {' ',' ',' '};
 int bufferCount = 0;
 
+//Pin connected to ST_CP of 74HC595
+int latchPin = 12;
+//Pin connected to SH_CP of 74HC595
+int clockPin = 13;
+////Pin connected to DS of 74HC595
+int dataPin = 11;
+//Master Reclear Pin
+int masterRec = 14;
+//Output Enable pin
+int outputEn = 15;
+
+
 void setup() {
   // put your setup code here, to run once:
-/* Set the baud rate for the hardware serial port */
-Serial.begin(9600);
-/* Set the baud rate for the software serial port */
-BluetoothSerial.begin(9600);
-/* Should respond with OK */
-BluetoothSerial.print("AT");
-waitForResponse();
-Serial.println("Bluetooth is recieving...:");
-BluetoothSerial.begin(9600);
+  /* Set the baud rate for the hardware serial port */
+  Serial.begin(9600);
+  /* Set the baud rate for the software serial port */
+  BluetoothSerial.begin(9600);  
+  /* Should respond with OK */
+  BluetoothSerial.print("AT");
+  waitForResponse();
+  Serial.println("Bluetooth is recieving...");
+
+  /*Set up register pins*/
+  //set pins to output because they are addressed in the main loop
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
+  pinMode(clockPin, OUTPUT);
+  pinMode(masterRec, OUTPUT);
+  pinMode(outputEn, OUTPUT);
+  //Reset output upon start up
+  digitalWrite(outputEn , HIGH);
+  digitalWrite(masterRec , LOW);
+  //flip above
+  digitalWrite(outputEn , LOW);
+  digitalWrite(masterRec , HIGH);
+
+
 }
 
-// Function to pass BlueTooth output through to serial port output
-void waitForResponse() {
-delay(1000);
-while (BluetoothSerial.available()) {
-Serial.write(BluetoothSerial.read());
-}
-Serial.write("\n");
-}
+
 
 void loop() {
   while (BluetoothSerial.available()) {
@@ -44,7 +64,28 @@ void loop() {
     int byteToSet = atoi(buffer);
     Serial.print("Byte recieved "); Serial.println(byteToSet);
     Serial.print("Binary form "); printBits(byteToSet); Serial.println("");
+    registerWriteByte(byteToSet);
   }
+}
+
+//This method writes the int (single byte value, example 255 = 0b11111111)
+//to shift register
+void registerWriteByte(int bytetowrite){
+    digitalWrite(latchPin, LOW);
+
+    shiftOut(dataPin, clockPin, LSBFIRST, bytetowrite);
+
+    digitalWrite(latchPin, HIGH);
+}
+
+
+// Function to pass BlueTooth output through to serial port output
+void waitForResponse() {
+delay(1000);
+while (BluetoothSerial.available()) {
+Serial.write(BluetoothSerial.read());
+}
+Serial.write("\n");
 }
 
 
