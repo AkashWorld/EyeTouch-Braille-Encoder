2761094336f0698f114ef452667ad1eb96584079
Khalid Akash <khalkash@gmail.com>
2018-03-23 04:01:36 -0400

    Support for four cells added

--- bluetooth/bluetooth_active/bluetooth_active.ino
+++ bluetooth/bluetooth_active/bluetooth_active.ino
@@ -1,3 +1,10 @@
+/*
+ * Author: Khalid Akash
+ */
+
+/*Reference for design can be found:
+ * https://www.arduino.cc/en/Tutorial/ShiftOut
+ */
 /* Include the software serial port library */
 #include <SoftwareSerial.h>
 /* to communicate with the Bluetooth module's TXD pin */
@@ -7,13 +14,13 @@
 /* Initialise the software serial port */
 SoftwareSerial BluetoothSerial(BT_SERIAL_TX, BT_SERIAL_RX);
 /*Buffer to keep characters recieved from bluetooth */
-char buffer1[] = {' ',' ',' '};
+char buffer1[] = {' ',' ',' ','\0'};
 int bufferCount1 = 0;
-char buffer2[] = {' ',' ',' '};
+char buffer2[] = {' ',' ',' ','\0'};
 int bufferCount2 = 0;
-char buffer3[] = {' ',' ',' '};
+char buffer3[] = {' ',' ',' ','\0'};
 int bufferCount3 = 0;
-char buffer4[] = {' ',' ',' '};
+char buffer4[] = {' ',' ',' ','\0'};
 int bufferCount4 = 0;
 
 //Pin connected to ST_CP of 74HC595
@@ -21,10 +28,7 @@ int latchPin = 12;
 //Pin connected to SH_CP of 74HC595
 int clockPin = 13;
 ////Pin connected to DS of 74HC595
-int dataPin1 = 11;
-int dataPin2 = 10;
-int dataPin3 = 9;
-int dataPin4 = 8;
+int dataPin = 11;
 //Master Reclear Pin
 int masterRec = 14;
 //Output Enable pin
@@ -45,10 +49,7 @@ void setup() {
   /*Set up register pins*/
   //set pins to output because they are addressed in the main loop
   pinMode(latchPin, OUTPUT);
-  pinMode(dataPin1, OUTPUT); 
-  pinMode(dataPin2, OUTPUT);
-  pinMode(dataPin3, OUTPUT);
-  pinMode(dataPin4, OUTPUT); 
+  pinMode(dataPin, OUTPUT); 
   pinMode(clockPin, OUTPUT);
   pinMode(masterRec, OUTPUT);
   pinMode(outputEn, OUTPUT);
@@ -69,22 +70,18 @@ void loop() {
     char recvChar = BluetoothSerial.read();
     Serial.print("Recieve Char: "); Serial.println(recvChar);
     if(bufferCount1 < 3){
-      Serial.print("Putting character into buffer1 "); Serial.println(bufferCount1);
       buffer1[bufferCount1] = recvChar;
       bufferCount1++;
     }
     else if(bufferCount2 < 3){
-      Serial.print("Putting character into buffer2 "); Serial.println(bufferCount2);
       buffer2[bufferCount2] = recvChar;
       bufferCount2++;
     }
     else if(bufferCount3 < 3){
-      Serial.print("Putting character into buffer3 "); Serial.println(bufferCount3);
       buffer3[bufferCount3] = recvChar;
       bufferCount3++;
     }
     else if(bufferCount4 < 3){
-      Serial.print("Putting character into buffer4 "); Serial.println(bufferCount4);
       buffer4[bufferCount4] = recvChar; 
       bufferCount4++;
     }
@@ -97,29 +94,19 @@ void loop() {
     int byteToSet2 = atoi(buffer2);
     int byteToSet3 = atoi(buffer3);
     int byteToSet4 = atoi(buffer4);
-    Serial.print("1st Byte recieved "); Serial.println(byteToSet1);
-    Serial.print("1st Binary form "); printBits(byteToSet1); Serial.println("");
-    Serial.print("2nd Byte recieved "); Serial.println(byteToSet2);
-    Serial.print("2nd Binary form "); printBits(byteToSet2); Serial.println("");
-    Serial.print("3rd Byte recieved "); Serial.println(byteToSet3);
-    Serial.print("3rd Binary form "); printBits(byteToSet3); Serial.println("");
-    Serial.print("4th Byte recieved "); Serial.println(byteToSet4);
-    Serial.print("4th Binary form "); printBits(byteToSet4); Serial.println("");
-    registerWriteByte(byteToSet1, dataPin1);
-    registerWriteByte(byteToSet2, dataPin2);
-    registerWriteByte(byteToSet3, dataPin3);
-    registerWriteByte(byteToSet4, dataPin4);
+    digitalWrite(latchPin, LOW);
+    registerWriteByte(byteToSet1);
+    registerWriteByte(byteToSet2);
+    registerWriteByte(byteToSet3);
+    registerWriteByte(byteToSet4);
+    digitalWrite(latchPin, HIGH);
   }
 }
 
 //This method writes the int (single byte value, example 255 = 0b11111111)
 //to shift register
-void registerWriteByte(int bytetowrite, int dataPin){
-    digitalWrite(latchPin, LOW);
-
+void registerWriteByte(int bytetowrite){
     shiftOut(dataPin, clockPin, LSBFIRST, bytetowrite);
-
-    digitalWrite(latchPin, HIGH);
 }
 
 
